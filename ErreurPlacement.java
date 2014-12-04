93e81056e44f818fdf784fe239295bdc9483aa3a
ElMelon <palaarc@hotmail.fr>
2014-12-04 10:14:10 +0100

    nouvo

---
+++ ErreurPlacement.java
@@ -0,0 +1,7 @@
+
+     class ErreurPlacement extends Exception {
+	
+	 public ErreurPlacement(){
+		    System.out.println("Vous ne pouvez pas placer votre bateau hors de la grille");
+		  }  
+}
---
+++ TirImpossible.java
@@ -0,0 +1,8 @@
+ 
+/* inutile*/
+class TirImpossible extends Exception {
+	
+	 public TirImpossible(){
+		    System.out.println("Vous ne pouvez pas tirer sur cette case");
+		  }  
+}
