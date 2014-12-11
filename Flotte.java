d8e745233d5f7cc6fcd0975b609d9a3586943b9e
Senthene <seycha.Sth@live.fr>
2014-12-11 11:56:01 +0100

    classe flotte complété

--- Flotte.java
+++ Flotte.java
@@ -1,15 +1,35 @@
+import java.util.ArrayList;
+
 
 public class Flotte {
 	
-	private Arraylist <Navire> Bateau;
+	private int robustesseFlotte = 0;
+	private ArrayList <Navire> Bateau;
 	
 	public Flotte (){
-	}
-	
-	public Flotte (Arraylist <Navire> b){
-		Bateau = b;
-	
-		
+		this.Bateau = new ArrayList <Navire> () ;
+		this.robustesseFlotte = robustesseFlotte;
 	}
 
+	public int getRobustesseFlotte() {
+		return robustesseFlotte;
+	}
+
+	public void setRobustesseFlotte(int robustesseFlotte) {
+		this.robustesseFlotte = robustesseFlotte;
+	}
+
+	public ArrayList<Navire> getBateau() {
+		return Bateau;
+	}
+
+	public void setBateau(ArrayList<Navire> bateau) {
+		Bateau = bateau;
+	}
+
+	
+		
+	
 }
+
+
