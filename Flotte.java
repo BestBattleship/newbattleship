1fc780896f436e34f5ee7d55aa27f9b0c5dee332
Senthene <seycha.Sth@live.fr>
2014-12-11 11:01:19 +0100

    pas de modif

--- Flotte.java
+++ Flotte.java
@@ -1,4 +1,15 @@
 
 public class Flotte {
+	
+	private Arraylist <Navire> Bateau;
+	
+	public Flotte (){
+	}
+	
+	public Flotte (Arraylist <Navire> b){
+		Bateau = b;
+	
+		
+	}
 
 }
--- Zone.java
+++ Zone.java
@@ -14,6 +14,7 @@ public class Zone {
 	}
 		
 		
+		
 
 		public int getX(){
 			
